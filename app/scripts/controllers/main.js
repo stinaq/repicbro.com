adc4a4e6a63493511ef4c75fe6ebbbf85fa1975f
Erik Kronberg <erik.a.kronberg@gmail.com>
2013-08-10 11:22:58 +0200

    Adds basic NSFW handling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@ angular.module('repicbro.controllers', ['repicbro.services'])
   .controller('MainCtrl', function ($scope, $http, Posts) {
     $scope.posts = [];
     $scope.current = null;
+    $scope.nsfw = false;
 
     Posts.get('funny', function (data) {
       angular.forEach(data.data.children, function (p) {
@@ -14,6 +15,10 @@ angular.module('repicbro.controllers', ['repicbro.services'])
 
     $scope.isCurrent = function (post) {
       return angular.equals($scope.current, post);
+    };
+
+    $scope.showNsfw = function () {
+      return !$scope.posts[0].over_18 || $scope.nsfw;
     };
 
   });
