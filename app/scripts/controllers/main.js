ee6b8f0ad7a7b898a261c183cf28ca999d6d9bb2
Erik Kronberg <erik.a.kronberg@gmail.com>
2013-08-10 17:09:37 +0200

    Copy over some functionality from old

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,13 @@ angular.module('repicbro.controllers', ['repicbro.services'])
     $scope.posts = [];
     $scope.current = null;
     $scope.nsfw = false;
+    $scope.index = 0;
 
     Posts.get('funny', function (data) {
       angular.forEach(data.data.children, function (p) {
         $scope.posts.push(p.data);
       });
-      $scope.current = $scope.posts[0];
+      $scope.current = $scope.posts[$scope.index];
     });
 
     $scope.isCurrent = function (post) {
@@ -21,4 +22,32 @@ angular.module('repicbro.controllers', ['repicbro.services'])
       return !$scope.posts[0].over_18 || $scope.nsfw;
     };
 
+    $scope.toggleNsfw = function () {
+      $scope.nsfw = !$scope.nsfw;
+    };
+
+    $scope.next = function () {
+      $scope.index++;
+      $scope.current = $scope.posts[$scope.index];
+    };
+
+    $scope.prev = function () {
+      $scope.index--;
+      $scope.current = $scope.posts[$scope.index];
+    };
+
+    $(window).keydown(function (e) {
+      if(e.which === 37 || e.which === 75) {
+        $scope.prev();
+        $scope.$apply();
+      }
+      else if (e.which === 39 || e.which === 74) {
+        $scope.next();
+        $scope.$apply();
+      }
+      else if (e.which === 78) {
+        $scope.toggleNsfw();
+        $scope.$apply();
+      }
+    });
   });
