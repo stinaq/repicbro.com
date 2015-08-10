12337bda80fd28a1156c0d5835f408afce244d0b
Stina Qvarnström <stinaqv@gmail.com>
2015-08-10 15:10:31 +0200

    Adding logging service

--- app/scripts/services/postsmanager.js
+++ app/scripts/services/postsmanager.js
@@ -4,6 +4,7 @@ angular.module('repicbro.services')
   .factory('PostsManager',
            function ($rootScope,
                      $sce,
+                     $log,
                      Posts,
                      Helpers) {
 
